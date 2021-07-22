2b1fbfeebddc63a47d90cb24bb6be652dc05e959
Joao Ortega <joaomarcello2008@gmail.com>
2021-07-22 02:35:33 -0300

    Add files via upload

---
+++ js/owl/setup.js
@@ -0,0 +1,16 @@
+$('.carousel').owlCarousel({
+    loop: false,
+    margin: 10,
+    nav: true,
+    responsive: {
+        0:{
+            items:1
+        },
+        700:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
